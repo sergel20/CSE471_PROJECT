91fca78621a6ed9380fd014dd6874d1ac9f859bc
pycrackersamin <saeeb.rahman.samin@g.bracu.ac.bd>
2026-08-08 20:32:55 +0600

    Connect booking route with backend

--- Backend/index.js
+++ Backend/index.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const connectDB = require('./database');
+
 const testRoutes = require('./routes/testRoutes');
-const resultRoutes = require('./routes/resultRoutes')
+const resultRoutes = require('./routes/resultRoutes');
+const bookingRoutes = require('./routes/bookingRoutes');
+
 const app = express();
 
 // Middleware
@@ -13,10 +16,12 @@ connectDB();
 // Mount Routes
 app.use('/api/tests', testRoutes);
 app.use('/api/results', resultRoutes);
+app.use('/api/bookings', bookingRoutes);
+
 // Test Route for base URL
 app.get('/', (req, res) => res.send('API Running'));
 
-const PORT = 1520 || process.env.PORT;
+const PORT = process.env.PORT || 1520;
 
 app.listen(PORT, () => {
   console.log(`🚀 Server is running on http://localhost:${PORT}`);
